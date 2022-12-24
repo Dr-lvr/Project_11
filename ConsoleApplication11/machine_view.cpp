d69f3b092bc798a67bab5a8f21b79e49fc65c3a5
Davide <dr_lvr_mail@yahoo.it>
2022-12-24 05:30:03 +0100

    Machine view DRAFT (pattern matching and tream) integrated

--- ConsoleApplication11/machine_view.cpp
+++ ConsoleApplication11/machine_view.cpp
@@ -1,31 +1,15 @@
-//DRAFT
 #include "machine_view.h"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
+
+#include <opencv2/opencv.hpp>
 #include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/core/core.hpp>
+
+#include <Windows.h>
 #include <iostream>
 #include <string>
 
-using namespace std;
-using namespace cv;
-
-//! [declare]
-/// Global Variables
-bool use_mask;
-Mat img; Mat templ; Mat mask; Mat result;
-const char* image_window = "Source Image";
-const char* result_window = "Result window";
-
-int match_method;
-int max_Trackbar = 5;
-//! [declare]
-
-/// Function Headers
-void MatchingMethod(int, void*);
-
-/**
- * @function main
- */
 int machine_view::machine_view_test()
 {
     //! [load_image]
@@ -33,8 +17,8 @@ int machine_view::machine_view_test()
     const std::string img1 = "C:/Users/david/Desktop/test/uno.jpg";
     const std::string img2 = "C:/Users/david/Desktop/test/otto.jpg";
 
-    img = imread(img1, IMREAD_COLOR);
-    templ = imread(img2, IMREAD_COLOR);
+    img = cv::imread(img1, cv::IMREAD_COLOR);
+    templ = cv::imread(img2, cv::IMREAD_COLOR);
     /*
     if (argc > 3) {
         use_mask = true;
@@ -43,40 +27,35 @@ int machine_view::machine_view_test()
 
     if (img.empty() || templ.empty() || (use_mask && mask.empty()))
     {
-        cout << "Can't read one of the images" << endl;
+        std::cout << "Can't read one of the images" << std::endl;
         return EXIT_FAILURE;
     }
     //! [load_image]
 
     //! [create_windows]
     /// Create windows
-    namedWindow(image_window, WINDOW_AUTOSIZE);
-    namedWindow(result_window, WINDOW_AUTOSIZE);
+    cv::namedWindow(image_window, cv::WINDOW_AUTOSIZE);
+    cv::namedWindow(result_window, cv::WINDOW_AUTOSIZE);
     //! [create_windows]
 
     //! [create_trackbar]
     /// Create Trackbar
     const char* trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
-    createTrackbar(trackbar_label, image_window, &match_method, max_Trackbar, MatchingMethod);
+    cv::createTrackbar(trackbar_label, image_window, &match_method, max_Trackbar, MatchingMethod);
     //! [create_trackbar]
 
     MatchingMethod(0, 0);
 
     //! [wait_key]
-    waitKey(0);
+    cv::waitKey(0);
     return EXIT_SUCCESS;
     //! [wait_key]
 }
-
-/**
- * @function MatchingMethod
- * @brief Trackbar callback
- */
-void MatchingMethod(int, void*)
+void machine_view::MatchingMethod(int, void*)
 {
     //! [copy_source]
     /// Source image to display
-    Mat img_display;
+    cv::Mat img_display;
     img.copyTo(img_display);
     //! [copy_source]
 
@@ -90,7 +69,7 @@ void MatchingMethod(int, void*)
 
     //! [match_template]
     /// Do the Matching and Normalize
-    bool method_accepts_mask = (TM_SQDIFF == match_method || match_method == TM_CCORR_NORMED);
+    bool method_accepts_mask = (cv::TM_SQDIFF == match_method || match_method == cv::TM_CCORR_NORMED);
     if (use_mask && method_accepts_mask)
     {
         matchTemplate(img, templ, result, match_method, mask);
@@ -102,20 +81,20 @@ void MatchingMethod(int, void*)
     //! [match_template]
 
     //! [normalize]
-    normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
+    normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
     //! [normalize]
 
     //! [best_match]
     /// Localizing the best match with minMaxLoc
-    double minVal; double maxVal; Point minLoc; Point maxLoc;
-    Point matchLoc;
+    double minVal; double maxVal; cv::Point minLoc; cv::Point maxLoc;
+    cv::Point matchLoc;
 
-    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat());
     //! [best_match]
 
     //! [match_loc]
     /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
-    if (match_method == TM_SQDIFF || match_method == TM_SQDIFF_NORMED)
+    if (match_method == cv::TM_SQDIFF || match_method == cv::TM_SQDIFF_NORMED)
     {
         matchLoc = minLoc;
     }
@@ -127,12 +106,79 @@ void MatchingMethod(int, void*)
 
     //! [imshow]
     /// Show me what you got
-    rectangle(img_display, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar(0, 255, 9), 2, 8, 0);
-    rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar(0, 255, 9), 2, 8, 0);
+    rectangle(img_display, matchLoc, cv::Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), cv::Scalar(0, 255, 9), 2, 8, 0);
+    rectangle(result, matchLoc, cv::Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), cv::Scalar(0, 255, 9), 2, 8, 0);
 
     imshow(image_window, img_display);
     imshow(result_window, result);
     //! [imshow]
 
     return;
+}
+
+cv::Mat machine_view::hwnd2mat(HWND hwnd)
+{
+    HDC hwindowDC, hwindowCompatibleDC;
+
+    int height, width, srcheight, srcwidth;
+    HBITMAP hbwindow;
+    cv::Mat src;
+    BITMAPINFOHEADER  bi;
+
+    hwindowDC = GetDC(hwnd);
+    hwindowCompatibleDC = CreateCompatibleDC(hwindowDC);
+    SetStretchBltMode(hwindowCompatibleDC, COLORONCOLOR);
+
+    RECT windowsize;    // get the height and width of the screen
+    GetClientRect(hwnd, &windowsize);
+
+    srcheight = windowsize.bottom;
+    srcwidth = windowsize.right;
+    height = windowsize.bottom / 1;  //change this to whatever size you want to resize to
+    width = windowsize.right / 1;
+
+    src.create(height, width, CV_8UC4);
+
+    // create a bitmap
+    hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
+    bi.biSize = sizeof(BITMAPINFOHEADER);    //http://msdn.microsoft.com/en-us/library/windows/window/dd183402%28v=vs.85%29.aspx
+    bi.biWidth = width;
+    bi.biHeight = -height;  //this is the line that makes it draw upside down or not
+    bi.biPlanes = 1;
+    bi.biBitCount = 32;
+    bi.biCompression = BI_RGB;
+    bi.biSizeImage = 0;
+    bi.biXPelsPerMeter = 0;
+    bi.biYPelsPerMeter = 0;
+    bi.biClrUsed = 0;
+    bi.biClrImportant = 0;
+
+    // use the previously created device context with the bitmap
+    SelectObject(hwindowCompatibleDC, hbwindow);
+    // copy from the window device context to the bitmap device context
+    StretchBlt(hwindowCompatibleDC, 0, 0, width, height, hwindowDC, 0, 0, srcwidth, srcheight, SRCCOPY); //change SRCCOPY to NOTSRCCOPY for wacky colors !
+    GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, src.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);  //copy from hwindowCompatibleDC to hbwindow
+
+    // avoid memory leak
+    DeleteObject(hbwindow);
+    DeleteDC(hwindowCompatibleDC);
+    ReleaseDC(hwnd, hwindowDC);
+
+    return src;
+}
+
+void machine_view::start_view() {
+    HWND hwndDesktop = GetDesktopWindow();
+    namedWindow("output", cv::WINDOW_NORMAL);
+    int key = 0;
+
+    while (key != 30)
+    {
+        cv::Mat src = hwnd2mat(hwndDesktop);
+        // you can do some image processing here
+        imshow("output", src);
+        key = cv::waitKey(30); // you can change wait time
+    }
+    ReleaseCapture();
+    cv::destroyAllWindows();
 }
--- ConsoleApplication11/machine_view.h
+++ ConsoleApplication11/machine_view.h
@@ -1,7 +1,27 @@
 #pragma once
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include <Windows.h>
 class machine_view
 {
 public:
 	int machine_view_test();
+	void start_view();
+private:
+	inline static bool use_mask;
+	inline static cv::Mat img;
+	inline static cv::Mat templ;
+	inline static cv::Mat mask;
+	inline static cv::Mat result;
+	inline static int match_method;
+	inline static const char* image_window = "Source Image";
+	inline static const char* result_window = "Result window";
+	static void MatchingMethod(int, void*);
+
+	int max_Trackbar = 5;
+	cv::Mat hwnd2mat(HWND hwnd);
 };
 
